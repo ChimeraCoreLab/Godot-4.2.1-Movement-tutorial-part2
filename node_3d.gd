9618c13b28f3940877eea36cc4390cc34b4293c9
Bas616 <pongsakornpongsuwakorn@gmail.com>
2025-04-17 15:13:05 +0700

    Initial commit: Godot 4.2.1 Movement Tutorial Part 2

---
+++ node_3d.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	'''if $Control/flashlight.is_pressed():
+		$Player/Neck/Head/Eyes/Camera/SpotLight3D
+	else:$Player/Neck/Head/Eyes/Camera/SpotLight3D.hide()
+		'''
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
